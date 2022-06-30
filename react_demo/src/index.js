be866fb7d8fe99a014e58ff5f00bf156ec2a0e41
tguliyev <tguliev9@gmail.com>
2022-06-30 16:42:54 +0400

    edit

--- react_demo/src/index.js
+++ react_demo/src/index.js
@@ -1,8 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <h1>Hello My Incredible Future!!!</h1>
+    <Page />
 );
+
+function Page() {
+    return (
+        <>
+            <Header />
+            <Content />
+            <Footer />
+        </>
+    )
+}
+
+function Header() {
+    return (
+        <header>
+            <nav className='navigation'>
+                <img src="logo192.png" alt="logo" />
+                <ul className='nav-items'>
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function Content() {
+    return (
+        <div>
+            <h1>Fun facts aboud React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintaned by Facebook</li>
+                <li>Powers thousands of enterprice apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
+
+function Footer() {
+    return (
+        <footer><small>&copy; 2022 Guliyev development. All rights reserved.</small></footer>
+    )
+}
+
+// function Fact() {
+//     return (
+//         <div>
+//             <img src='logo192.png' alt="logo"/>
+//             <h1></h1>
+//             
+//         </div>
+//     )
+// }
